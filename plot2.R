0bac152449ccab3e358d87f3985991dc588511e8
pavithra <thilini.gunasekara@gmail.com>
2014-06-07 18:26:55 +0530

    Adding plot2 resources

---
+++ plot2.R
@@ -0,0 +1,12 @@
+##Loading relevant data to R
+options(stringsAsFactors=F)
+dataset <- read.table("household_power_consumption.txt",sep=";",na.strings="?",skip=66636,nrow=2880,quote="",header=TRUE,colClasses = "character")
+colnames(dataset) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+transform.data <- transform(dataset,Date=as.Date(strptime(Date,"%d/%m/%Y")),Time=strptime(paste(Date,Time,sep=" "),"%d/%m/%Y %H:%M:%S"))
+
+##Generating the plot
+plot(transform.data$Time,as.numeric(transform.data$Global_active_power),type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+##Creating the PNG file
+dev.copy(png,file="plot2.png")
+dev.off()
